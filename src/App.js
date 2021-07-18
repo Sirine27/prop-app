a3ec3c08759de2fec4c325cb878887007b5ee19e
sirine27 <mathlouthi.sirine124@gmail.com>
2021-07-18 06:01:51 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Profile from './profile/Profile';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <Profile />
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
